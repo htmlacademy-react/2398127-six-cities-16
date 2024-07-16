5b1acaa8629f3f375c675eb6b01c8785ee5910c9
ArsiRex <arsirex@yandex.ru>
2024-07-16 15:06:55 +0300

    Опишет маршруты в приложении(WIP)

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Offer from '../../pages/offer/offer';
+//import Favorites from '../../pages/favorites/favorites';
+import PageNotFound from '../../pages/page-not-found/page-not-found';
 
 type AppProps = {
   offersCount: number;
@@ -6,7 +12,30 @@ type AppProps = {
 
 function App({offersCount}: AppProps): JSX.Element {
   return (
-    <Main offersCount={offersCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<Main offersCount={offersCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<Offer />}
+        />
+        {/* <Route
+          path={AppRoute.Favorites}
+          element={}
+        /> */}
+        <Route
+          path="*"
+          element={<PageNotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
