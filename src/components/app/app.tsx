7d6fbe10d47524f1f4deddf0e2af2e025a88103e
ArsiRex <arsirex@yandex.ru>
2024-07-16 15:28:51 +0300

    Реализует приватный маршрут для Favorites

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,11 @@
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
-import {AppRoute} from '../../const';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import Main from '../../pages/main/main';
 import Login from '../../pages/login/login';
 import Offer from '../../pages/offer/offer';
-//import Favorites from '../../pages/favorites/favorites';
+import Favorites from '../../pages/favorites/favorites';
 import PageNotFound from '../../pages/page-not-found/page-not-found';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   offersCount: number;
@@ -26,10 +27,16 @@ function App({offersCount}: AppProps): JSX.Element {
           path={AppRoute.Offer}
           element={<Offer />}
         />
-        {/* <Route
+        <Route
           path={AppRoute.Favorites}
-          element={}
-        /> */}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
         <Route
           path="*"
           element={<PageNotFound />}
