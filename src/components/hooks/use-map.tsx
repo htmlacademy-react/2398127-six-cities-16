4db58a87d7aec588b3a06cb14d324f1e59e055ed
ArsiRex <arsirex@yandex.ru>
2024-08-13 16:09:56 +0300

    fix

--- src/components/hooks/use-map.tsx
+++ src/components/hooks/use-map.tsx
@@ -4,6 +4,7 @@ import { Map, TileLayer } from 'leaflet';
 
 function useMap(mapRef: MutableRefObject<HTMLElement | null>, city: City): Map | null {
   const [map, setMap] = useState<Map | null>(null);
+
   const isRenderedRef = useRef<boolean>(false);
 
   useEffect(() => {
