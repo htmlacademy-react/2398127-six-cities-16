39b6c6a9d46b0a1e0e21a56c6e85283f4bdce353
ArsiRex <arsirex@yandex.ru>
2024-08-25 16:52:46 +0300

    Обновит компонент AuthScreen.

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,7 +1,28 @@
 import Logo from '../../components/logo/logo';
 import { Helmet } from 'react-helmet-async';
+import {useRef, FormEvent} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {useAppDispatch} from '../../components/hooks';
+import {loginAction} from '../../store/api-actions';
+import {AppRoute} from '../../const.ts';
 
 function Login(): JSX.Element {
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const submitFormHandler = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if(emailRef.current !== null && passwordRef !== null) {
+      dispatch(loginAction({
+        email: emailRef.current.value,
+        password: passwordRef.current.value
+      }));
+    }
+  };
   return(
     <div className="page page--gray page--login">
       <Helmet>
@@ -21,16 +42,30 @@ function Login(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form
+              className="login__form form" action="#" method="post"
+              onSubmit={submitFormHandler}
+            >
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
+                <input
+                  className="login__input form__input" type="email" name="email" placeholder="Email" required
+                  ref={emailRef}
+                />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
+                <input
+                  className="login__input form__input" type="password" name="password" placeholder="Password" required
+                  ref={passwordRef}
+                />
               </div>
-              <button className="login__submit form__submit button" type="submit">Sign in</button>
+              <button
+                className="login__submit form__submit button" type="submit"
+                onClick={() => navigate(AppRoute.Root)}
+              >
+                Sign in
+              </button>
             </form>
           </section>
           <section className="locations locations--login locations--current">
