35d44e78ade20e746d3d49d3ba70c2e0d8f0d374
ArsiRex <arsirex@yandex.ru>
2024-09-02 13:40:59 +0300

    Исправление ошибок

--- src/store/comments-data/selectors.ts
+++ src/store/comments-data/selectors.ts
@@ -2,5 +2,5 @@ import { NameSpace } from '../../const';
 import { Comment } from '../../types/comment';
 import { State } from '../../types/state';
 
-export const getComments = (state: State): Comment[] =>
+export const getComments = (state: Pick<State, NameSpace.Comments>): Comment[] =>
   state[NameSpace.Comments].comments;
--- src/store/errors-process/selectors.ts
+++ src/store/errors-process/selectors.ts
@@ -1,4 +1,4 @@
 import { NameSpace } from '../../const';
 import { State } from '../../types/state';
 
-export const getErrorMessage = (state: State) => state[NameSpace.Errors].error;
+export const getErrorMessage = (state: Pick<State, NameSpace.Errors>) => state[NameSpace.Errors].error;
